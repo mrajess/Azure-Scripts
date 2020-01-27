7aa6c4edf3e75339f323172cdcb3848b7eeecbd3
mrajess <45776895+mrajess@users.noreply.github.com>
2020-01-27 08:50:00 -0600

    Create azureScript.ps1

---
+++ PowerShell/get-list-of-subnet-role-assignments/azureScript.ps1
@@ -0,0 +1,32 @@
+# The name of the role you're wanting to query on. Format follows "Owner".
+$queriedRole = "Owner"
+
+# Name of the file and path for which you want said file exported to. Format follows "C:\exampleDirectory\exampleFile.csv".
+
+$exportPath = "C:\Users\mrajess\Documents\TempOutput\results.csv"
+
+#Connect-AzAccount
+
+$subscriptions = Get-AzSubscription
+
+foreach ($sub in $subscriptions) {
+    $subId = $sub.subscriptionId
+    Set-AzContext -Subscription $subId
+    $graphQuery = "where type == ""microsoft.network/virtualnetworks"" and subscriptionId == ""$subId"""
+    $graphResult = Search-AzGraph -Query $graphQuery
+
+    foreach ($result in $graphResult.Properties.subnets) {
+        $rolenames = @()
+        $roles = Get-AzRoleAssignment -RoleDefinitionName $queriedRole -Scope $result.id
+        foreach ($role in $roles) {
+            $rolenames += $role.DisplayName
+        }
+        $subnet = Get-AzVirtualNetworkSubnetConfig -ResourceId $result.id
+        [pscustomobject]@{
+            SubnetId      = $result.id
+            SubnetName    = $result.name
+            SubnetAddress = $subnet.AddressPrefix | Out-string
+            AssignedRoles = (@($rolenames) | Out-String).Trim()
+        } | Export-Csv -notype -Path $exportPath -Append
+    }
+}
